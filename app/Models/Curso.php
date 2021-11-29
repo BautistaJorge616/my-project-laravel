bf6ec74948bf2bb7e13aaece009e40ff19d9c363
BautistaJorge616 <bautistajorge616@gmail.com>
2021-11-29 13:19:53 -0600

    Bases de datos y laravel

---
+++ app/Models/Curso.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Curso extends Model
+{
+    use HasFactory;
+
+    //Definir que tabla quiero que administre
+    //protected $table = 'cursos';
+}
